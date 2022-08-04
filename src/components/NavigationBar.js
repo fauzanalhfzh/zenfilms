17f10f16210680c8c119d0970c0d78a44231f557
fauzanalhfzh <zenbangga@gmail.com>
2022-08-04 13:41:59 +0700

    mengubah Nama Navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,7 @@ const NavigationBar = () => {
         <div>
         <Navbar variant="dark">
             <Container>
-                <Navbar.Brand>ZENFILMS</Navbar.Brand>
+                <Navbar.Brand class="navbarNama">ZENFILMS</Navbar.Brand>
                 <Nav>
                 <Nav.Link href="#trending">TRENDING</Nav.Link>
                 <Nav.Link>SUPERHERO</Nav.Link>
